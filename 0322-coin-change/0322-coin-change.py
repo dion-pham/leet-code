f75b840302739bb1c4c4d70bb5ae81134aed2fe5
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-06-20 16:03:24 -0700

    Time: 1379 ms (59.22%), Space: 16.6 MB (89.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,40 +1,12 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        if self._coinChange(coins, amount,{}) == float('inf'):
-            return -1
-        else: 
-            return self._coinChange(coins, amount, {})
-    
-    def _coinChange(self, coins, amount, memo):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount +1)
+        dp[0] = 0
         
-#      recursion: amount - coin
-#      base case: if amount == 0, return 0
-#           w each recursive step, return 1 
-        
-        if amount in memo: 
-            return memo[amount]
-
-        if amount == 0: 
-            return 0
-        
-        if amount < 0: 
-            return float("inf")
-#         return infinity because if you return -1 here, you are return an invalid answer with 1 subtracted from it
-        
-        min_coins = float("inf")
-        
-        for coin in coins: 
-            num_coins  = 1 + self._coinChange(coins, amount - coin, memo)
-            if num_coins < min_coins: 
-                min_coins = num_coins
-        
-            
-        memo[amount] = min_coins
-        return memo[amount]
-        
-       
+        for a in range(1,amount+1):
+            for c in coins: 
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a-c])
+                    
+        return dp[amount] if dp[amount] != amount + 1 else -1
+                    
