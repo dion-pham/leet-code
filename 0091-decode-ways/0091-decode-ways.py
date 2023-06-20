53fa2b58915f088716cc7085e7ae65ecf535b10e
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-06-20 16:24:59 -0700

    Time: 59 ms (7.62%), Space: 16.4 MB (29.34%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,29 +1,24 @@
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def numDecodings(self, s: str) -> int:
         
-#   use the indeces from the alphabet
-#   find all possible combinations of the string that could create the number
-#   use dynamic programming
-#   12 = how many ways to form 1 + 2 and how many ways to form 12 
-
-
-        dp = { len(s): 1 }
-    
-        def dfs(self, i):
-            if i in dp:
+        
+        dp = {len(s):1}
+        
+        def dfs(i):
+            if i in dp: 
                 return dp[i]
             
-            if s[i] == '0': return 0
+            if s[i] == '0':
+                return 0
             
-            res = dfs(self, i+1)
-            if (i+1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i+1] in '0123456')):
-                res += dfs(self,i+2)
+            res = dfs(i+1)
+            
+#             if the i index value is less than len(s), then its inbounds
+            if i +1 < len(s) and (s[i] == '1' or
+                s[i] =='2' and s[i+1] in '0123456'):
+                res += dfs(i+2)
             
             dp[i] = res
             return res
-        
-        return dfs(self, 0)
+                
+        return dfs(0)
