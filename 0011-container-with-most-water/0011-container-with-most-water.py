81c62d5e66dfeecf7afecbba67895393f68e7b92
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-06-20 13:09:24 -0700

    Time: 589 ms (78.67%), Space: 23.9 MB (54.78%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,23 +4,26 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-#        height == height[i]
-       # width == i
-#     you need the difference in i between both to be large
-#   and you can only take min(height[i], height[j]) for the height
 
-# iterate through
+#   two pointers: left and right
+#   area: length * height
+#   length == difference in i between your left/right pointers
+#   height == min(height[left], height[right])
+#   max variable for area
+#   move left or right pointer according to: which is the lower between the two 
 
-        res = 0
-        l, r = 0, len(height) -1
-        while l < r: 
-            area = (r-l) * min(height[l], height[r])
-            res = max(res,area)
-            if height[l] < height[r]:
-                l += 1
-            elif height[l] > height[r]:
-                r -= 1  
+        left, right = 0, len(height) - 1
+        area = 0
+
+        while left != right: 
+            tempArea = (right - left) * min(height[left], height[right])
+            if tempArea > area: 
+                area = tempArea
+
+            if height[left] < height[right]: 
+                left += 1 
             else: 
-                r -= 1
-                
-        return res
+                right -= 1
+
+
+        return area 
