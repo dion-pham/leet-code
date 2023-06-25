d28f497e94fc24ab965e778cdf1b0b4bfc4ca224
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-06-25 16:33:45 -0700

    Time: 67 ms (59.32%), Space: 20.4 MB (41.28%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -15,17 +15,41 @@ class Solution:
 # traverse the tree, keep track of node values
     # append to a list
     # find the kth smallest value according to that list
-# ^^ the method above can be implemented using recursion: DFS with inorder traversal 
+    
+    
+#  the method above can be implemented using recursion: DFS with inorder traversal 
 
-        res =[]
+#         res =[]
 
-        def traverse(root):
-            if root: 
-                traverse(root.left)
-                res.append(root.val)
-                traverse(root.right)
+#         def traverse(root):
+#             if root: 
+#                 traverse(root.left)
+#                 res.append(root.val)
+#                 traverse(root.right)
 
-        traverse(root)
-        return res[k-1]
+#         traverse(root)
+#         return res[k-1]
 
 
+        n = 0
+        stack = []
+        current = root
+        
+        
+        
+        while current or stack: 
+            while current: 
+#                 keep going left before visiting current node
+                stack.append(current)
+                current = current.left
+        
+            current = stack.pop()
+            n += 1
+            if n ==k: 
+                return current.val
+#             go right once you reach a left null value
+            current = current.right
+            
+            
+    
+                
