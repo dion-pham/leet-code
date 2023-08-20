4bceb69f713ae57f5fd458e24ca6f36efd0519e5
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-08-20 15:40:07 -0700

    Time: 13 ms (69.89%), Space: 13.3 MB (22.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,22 +1,26 @@
 class Solution(object):
-    def climbStairs(self, n):
+    def climbStairs(self,n):
+        return self._climbStairs(n,{})
+    def _climbStairs(self, n, memo):
         """
         :type n: int
         :rtype: int
         """
-
-        # Define the dfs function with memoization
-        def dfs(n, memo):
-            if n < 0:
-                return 0
-            if n == 0:
-                return 1
-            if n in memo:
-                return memo[n]
-
-            # Calculate the result for n and store it in the memo dictionary
-            memo[n] = dfs(n - 1, memo) + dfs(n - 2, memo)
+        
+        #memoization: to allow for faster complexity 
+        
+        #put changing value in memo
+        if n in memo: 
             return memo[n]
-
-        # Call the dfs function with an empty memo dictionary
-        return dfs(n, {})
+        
+        #given a total. see how many steps if takes to get to the end
+        #recursion - each recursive step, you are subtracting either 1 or 2 from n
+        #base case: when n = 0, return 1 to bubble back up to the top of the call stack. indicating 1 valid way
+        
+        if n == 0: 
+            return 1
+        if n < 0: 
+            return 0
+        
+        memo[n] = self._climbStairs( n-1, memo) + self._climbStairs( n-2, memo)
+        return memo[n]
