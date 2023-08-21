b2585028759bff172e4f4de634005a6107044e60
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-08-20 20:27:38 -0700

    Time: 28 ms (98.88%), Space: 16.4 MB (52.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,22 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #stack data structure
+        #use a hash map for every pair of opening to closing character
+        
+        #append all openers to the stack,
+            #can add as many consecutive openers as you want PROVIDED that there are enough closers in the right                order
         stack = []
-        brackets = {
-            '[': ']',
-            '{' : '}',
-            '(' : ')'
-        }
-  
-        for char in s: 
-            if char in brackets: 
-                stack.append(brackets[char])
-            else: 
-                if stack and char == stack[-1]:
+        closeToOpen = {')':'(', ']':'[','}':'{'}
+        
+        for char in s:
+            if char in closeToOpen: 
+                if stack and stack[-1] == closeToOpen[char]:
                     stack.pop()
                 else: 
-                    return False
-      
-        return len(stack) == 0
+                    return False 
+            else: 
+                stack.append(char)
+        if not stack: 
+            return True
+        else: 
+            return False
