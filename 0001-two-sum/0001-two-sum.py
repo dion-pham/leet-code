076ae615b86782f62b322b5b60f1607a18f97830
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-08-21 23:33:21 -0700

    Time: 69 ms (74.22%), Space: 17.7 MB (14.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    
-        hash = {}
-        for index, num in enumerate(nums):
-            # print(index,num)
-            difference = target - num
-            if difference not in hash: 
-                hash[num] = index 
-            else: 
-                return [hash[difference], index]
+        #iterate through the array
+        #{0:7, index: difference}
+        #add the number at the index as a key, difference as a value
+            #for next index, if target-index is equal to a key in hash, then you have your two return values 
             
-        return 
-    
-# iterate through, target minus the index. we will get the difference == 7 for the index of 0
-#  {7: 0 , 2: 1}
+        differenceHash = {}
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if nums[i] in differenceHash: 
+                return [i, differenceHash[nums[i]]]
+            if difference not in differenceHash: 
+                differenceHash[difference] = i
+
+            
+            
+            
+#    {7:0,}
+#   {3:0, 4:1, }
