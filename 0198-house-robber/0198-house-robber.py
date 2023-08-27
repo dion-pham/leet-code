702344c93ecbe36ef2ca600e6d516a860ffaa78d
Dion Pham <68131808+dion-pham@users.noreply.github.com>
2023-08-27 13:25:35 -0700

    Time: 38 ms (79.81%), Space: 16.4 MB (36.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,25 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-#     dp: take or dont take first index
-#     take every other index from that point on
-#     return the max from either sum
-
-        def _rob(self, nums, i,memo):
-            if i in memo: 
-                return memo[i]
+class Solution:
+    def rob(self,nums):
+        return self._rob(nums,0,{})
+    def _rob(self, nums: List[int], i, memo) -> int:
+        #start at the first index, take it
+            #perform a recursive step that includes the first element
+            #perform a recursive step that excludes the first element 
             
-            if i >= len(nums): return 0
             
-            include = nums[i] + _rob(self,nums, i+2,memo)
-            exclude = _rob(self,nums, i+1,memo)
-           
-            
-            memo[i] = max(include, exclude)
+        # if len(nums) == 0: 
+        #     return 0
+        
+        if i in memo:
             return memo[i]
-        return _rob(self,nums,0,{})
+        
+        if i >= len(nums):
+            return 0
+        
+        include = nums[i] + self._rob(nums, i+2,memo)
+        exclude = self._rob(nums, i+1, memo)
+        
+        memo[i] = max(include, exclude)
+        return memo[i]
+        
+        
